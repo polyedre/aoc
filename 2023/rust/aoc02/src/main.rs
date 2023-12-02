e34d2a28c3a68855cf87c4a637cee06b5cc5cd31
Lucas Henry <lucas.henry@cdiscount.com>
2023-12-02 11:21:57 +0100

    Add 2023/aoc02 part 2

--- 2023/rust/aoc02/src/main.rs
+++ 2023/rust/aoc02/src/main.rs
@@ -5,7 +5,8 @@ const GREEN_MAX: u32 = 13;
 const BLUE_MAX: u32 = 14;
 
 fn main() {
-    let mut total = 0;
+    let mut total_part_1 = 0;
+    let mut total_part_2 = 0;
 
     for (identifier, line) in io::stdin().lines().enumerate() {
 
@@ -14,6 +15,10 @@ fn main() {
         let line = line.unwrap();
         let mut parse_result = line.split(':');
         let _game_str = parse_result.next();
+
+        let mut min_red = 0;
+        let mut min_green = 0;
+        let mut min_blue = 0;
 
         for subset in parse_result.next().unwrap().split(';') {
             for single_cube_draw in subset.trim().split(',') {
@@ -24,22 +29,33 @@ fn main() {
                 let color = cube.next().unwrap();
 
                 match color {
-                    "blue" => { if amount > BLUE_MAX { possible = false } },
-                    "red" => { if amount > RED_MAX { possible = false } },
-                    "green" => { if amount > GREEN_MAX { possible = false } },
+                    "red" => {
+                        min_red = std::cmp::max(min_red, amount);
+                        if amount > RED_MAX { possible = false }
+                    },
+                    "green" => {
+                        min_green = std::cmp::max(min_green, amount);
+                        if amount > GREEN_MAX { possible = false }
+                    },
+                    "blue" => {
+                        min_blue = std::cmp::max(min_blue, amount);
+                        if amount > BLUE_MAX { possible = false }
+                    },
                     _ => panic!("Color {} uncovered", color)
                 }
-
             }
 
             println!("{}", subset);
         }
 
+        total_part_2 += min_red * min_green * min_blue;
+
         if possible {
             println!("Id is {}", identifier + 1);
-            total += identifier + 1;
+            total_part_1 += identifier + 1;
         }
     }
 
-    println!("{}", total)
+    println!("Total part 1: {}", total_part_1);
+    println!("Total part 2: {}", total_part_2);
 }

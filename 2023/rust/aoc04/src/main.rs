6b6c4f59cfbf5a7e04c771c546d6e95e95394175
Lucas Henry <lucas.henry@cdiscount.com>
2023-12-04 11:53:22 +0100

    Add 2023/aoc04 part 2

--- 2023/rust/aoc04/src/main.rs
+++ 2023/rust/aoc04/src/main.rs
@@ -1,7 +1,16 @@
 use std::io;
 
+#[derive(Clone, Debug)]
+struct Scratchcard {
+    number_of_copies: u32,
+    number_of_wins: u32
+}
+
 fn main() {
     let mut total = 0;
+    let mut total_part_2 = 0;
+
+    let mut scratchcards: Vec<Scratchcard> = Vec::new();
 
     for line in io::stdin().lines() {
         let line = line.unwrap();
@@ -22,10 +31,26 @@ fn main() {
 
         println!("Total number of win: {}", number_of_win);
 
+        scratchcards.push(Scratchcard { number_of_copies: 1, number_of_wins: number_of_win });
+
         if number_of_win > 0 {
             total += 2_u32.pow(number_of_win - 1);
         }
     }
 
+    for current_id in 0..scratchcards.len() {
+
+        let scratchcard = scratchcards[current_id].clone();
+
+        for offset in 1..=(scratchcard.number_of_wins as usize) {
+            scratchcards[current_id + offset].number_of_copies += scratchcard.number_of_copies;
+        }
+
+        println!("Card {} has {} instances", current_id + 1, scratchcard.number_of_copies);
+
+        total_part_2 += scratchcard.number_of_copies;
+    }
+
     println!("Total part 1: {}", total);
+    println!("Total part 2: {}", total_part_2);
 }

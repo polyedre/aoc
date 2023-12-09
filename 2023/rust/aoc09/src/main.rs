411d14573828b1f32771e890563ca385f21cd661
Lucas Henry <lucas.henry@cdiscount.com>
2023-12-09 10:21:53 +0100

    Add 2023/aoc09 part 2

--- 2023/rust/aoc09/src/main.rs
+++ 2023/rust/aoc09/src/main.rs
@@ -13,20 +13,38 @@ fn get_next_number(numbers: &Vec<i32>) -> i32 {
     return numbers.last().unwrap() + new_difference;
 }
 
+
+fn get_previous_number(numbers: &Vec<i32>) -> i32 {
+    if numbers.iter().sum::<i32>() == 0 { return 0 }
+
+    let mut differences: Vec<i32> = Vec::new();
+
+    for index in 0..(numbers.len() - 1) {
+        differences.push(numbers[index + 1] - numbers[index]);
+    }
+
+    let new_difference = get_previous_number(&differences);
+    return numbers.first().unwrap() - new_difference;
+}
+
 fn main() {
-    let mut total = 0;
+    let mut total_part_1 = 0;
+    let mut total_part_2 = 0;
 
     for line in io::stdin().lines() {
         let line = line.unwrap();
 
         let numbers: Vec<i32> = line.split_whitespace().map(|num| num.parse().unwrap()).collect();
 
-        let new_number = get_next_number(&numbers);
+        let next_number = get_next_number(&numbers);
+        let previous_number = get_previous_number(&numbers);
 
-        println!("Sequence {numbers:?} might end with {new_number}");
+        println!("Sequence {previous_number} {numbers:?} {next_number}");
 
-        total += new_number;
+        total_part_1 += next_number;
+        total_part_2 += previous_number;
     }
 
-    println!("Part 1 solution is {total}");
+    println!("Part 1 solution is {total_part_1}");
+    println!("Part 2 solution is {total_part_2}");
 }

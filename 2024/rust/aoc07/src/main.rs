0006ff84ea153200f5845ea49fa1a4050ad4bab7
Polyedre <polyedre@disroot.org>
2024-12-15 12:44:44 +0100

    Parse left to go from 180ms to 1.5ms

--- 2024/rust/aoc07/src/main.rs
+++ 2024/rust/aoc07/src/main.rs
@@ -2,16 +2,16 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::{fs::File, time::Instant};
 
-fn is_possible(equation: &[u64], sum: u64, operations: &[fn(u64, u64) -> u64]) -> bool {
+fn is_possible(equation: &[u64], sum: u64, operations: &[fn(u64, u64) -> Option<u64>]) -> bool {
     match equation {
-        [] => sum == 0,
-        [elem] => *elem == sum,
-        [first, ..] if *first > sum => false,
-        [first, second, remainder @ ..] => {
+        [] => panic!("Should not happen"),
+        [elem] => sum == *elem,
+        [remainder @ .., last] => {
             operations.iter().any(|op| {
-                let next = op(*first, *second);
-                let new_equation: Vec<u64> = std::iter::once(next).chain(remainder.iter().cloned()).collect();
-                is_possible(&new_equation, sum, operations)
+                match op(sum, *last) {
+                    Some(sum) => is_possible(remainder, sum, operations),
+                    None => false,
+                }
             })
         }
     }
@@ -38,19 +38,14 @@ fn main() {
     let filename: &str = &std::env::args().nth(1).expect("Expected filename");
     let input = parse_input(filename);
 
-    let add = |a: u64, b: u64| a + b;
-    let multiply = |a: u64, b: u64| a * b;
-    let concat: fn(u64, u64) -> u64 = |a, b| {
-        let mut offset = 1;
-
-        while offset <= b {
-            offset *= 10;
-        }
-
-        a * offset + b
+    let subs: fn(u64, u64) -> Option<u64> = |a, b| if a > b { Some(a - b) } else { None };
+    let divide: fn(u64, u64) -> Option<u64> = |a, b| if a % b == 0 { Some(a / b) } else { None };
+    let unconcat: fn(u64, u64) -> Option<u64> = |a, b| {
+        let mask = 10u64.pow(b.ilog10() + 1);
+        if a % mask == b { Some(a / mask) } else { None }
     };
 
-    let operations: &[fn(u64, u64) -> u64] = &[add, multiply, concat];
+    let operations: &[fn(u64, u64) -> Option<u64>] = &[subs, divide, unconcat];
 
     let now = Instant::now();
     let part1: u64 = input.iter()
@@ -62,7 +57,6 @@ fn main() {
         .sum();
     let elapsed = now.elapsed();
 
-    println!("Optimized:");
     println!("Part 1: {}", part1);
     println!("Part 2: {}", part2);
     println!("Elapsed: {:.2?}", elapsed);
